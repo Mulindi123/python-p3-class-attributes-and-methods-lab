221eb6bfb43bb42fc59fda1a8d74151b93827e02
Mulindi123 <medrine.mulindi@gmail.com>
2023-09-29 23:02:32 +0300

    Ammended assignment

--- lib/song.py
+++ lib/song.py
@@ -11,22 +11,23 @@ class Song:
         self.artist = artist
         self.genre = genre
         Song.add_song_to_count()
-        self.add_to_genres()
-        self.add_to_artists()
+        self.add_to_genres(self.genre)
+        self.add_to_artists(self.artist)
         
 
     @classmethod
     def add_song_to_count(cls):
         cls.count += 1
 
-   
-    def add_to_genres(self):
-        if self.genre not in Song.genres:
-            Song.genres.append(self.genre)
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
 
-    def add_to_artists(self):
-        if self.artist not in Song.artists:
-            Song.artists.append(self.artist)
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
     @classmethod
     def add_to_genre_count(cls):
@@ -46,12 +47,16 @@ class Song:
     def all_songs(cls): 
         return [song for song in cls.__subclasses__()]
 
-ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
-another_song = Song("Song 2", "Blur", "Rock")
-yet_another_song = Song("Song 3", "Johnny Cash", "Country")
-Song.add_to_genre_count()
-print(Song.add_to_artist_count())
+# ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+# another_song = Song("Song 2", "Blur", "Rock")
+# yet_another_song = Song("Song 3", "Johnny Cash", "Country")
+# Song.add_to_genre_count()
+# print(Song.add_to_artist_count())
 
 # print(Song.genre_count) 
 # print(Song.artist_count) 
 
+song1 = Song("Song 1", "Artist 1", "Pop")
+song2 = Song("Song 2", "Artist 2", "Rock")
+song3 = Song("Song 3", "Artist 3", "Hip Hop")
+print(Song.count)
